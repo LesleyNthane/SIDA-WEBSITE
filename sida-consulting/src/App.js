46ecad11016c695af8dd4042d08118046d1222f0
LesleyNthane <nthanelesley2001@gmail.com>
2024-08-04 20:23:42 +0200

    Busy with the About component interms of responsivenes

--- sida-consulting/src/App.js
+++ sida-consulting/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from './Component/NavBar';
 import { Banner } from './Component/Banner';
+import { About } from './Component/About';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Banner />
+      <About />
     </div>
   );
 }
