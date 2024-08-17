b91d2cb2ed7c7bce2a50f8192b37038ac24dd69e
LesleyNthane <nthanelesley2001@gmail.com>
2024-08-17 23:57:03 +0200

    Put pictures for the contact form and fix fonts

--- sida-consulting/src/Component/Contact.js
+++ sida-consulting/src/Component/Contact.js
@@ -2,13 +2,21 @@ import phoneicon from "../Assets/img/icons8-phone-30.png";
 import emailicon from "../Assets/img/icons8-email-30.png";
 import officeicon from "../Assets/img/icons8-office-30.png";
 import logo from '../Assets/img/SIDA-LOGO.png';
+import pic1 from '../Assets/img/Sida-const-pic-1.jpg';
+import pic2 from '../Assets/img/Sida-const-pic-3.jpg';
+import pic3 from '../Assets/img/Sida-const-pic-4.jpg';
 
 export const Contact = () => {
     return (
         <section class="contact" id="contact">
             <h4 class="company-name">SIDA CONSULTING & PROJECT MANAGEMENT</h4>
             <h1 class="title-contact col-4">Contact Us</h1>
-            <img src="" alt="company-pc" />
+            <div className="pics col-12">
+                <img src={pic1} className="col-xs-4 col-sm-4 col-md-4 col-lg-4" alt="company-pc" />
+                <img src={pic2} className="col-xs-4 col-sm-4 col-md-4 col-lg-4" alt="company-pc" />
+                <img src={pic3} className="col-xs-4 col-sm-4 col-md-4 col-lg-4" alt="company-pc" />
+
+            </div>
             <h4 class="Contact-form col-4" id="contact-form">CONTACT FORM<hr /></h4>
             <div className="row" id="input-boxes">
                 <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 input-box">
@@ -23,7 +31,7 @@ export const Contact = () => {
                     <label className="col-1">Email</label><br />
                     <input type="email" className="col-12 field" placeholder="Enter your email" required />
                 </div>
-                <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 input-box">
+                <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 input-box" id="text-area1">
                     <label className="col-1">Message</label><br />
                     <textarea name="" id="" className="col-12 field message" placeholder="Enter your message" required />
                 </div>
